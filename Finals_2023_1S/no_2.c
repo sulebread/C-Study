c2d572038d5b53e7fbc8bca57be27d381498c887
haky <57565164+sulebread@users.noreply.github.com>
2023-06-11 16:02:11 +0900

    2번 문제 답
    1. 에러 원인
    - 함수 매개변수와 전달인자 개수 불일치.
    - main함수 스펠링 잘못 씀.
    
    2. 예상결과
    "가장 큰 수는 0입니다"
    - main 함수 내 printf에서의 max는 main 함수에서 선언한 int max 변수를 의미한다.
    - 전역 변수와 같은 변수명을 쓰는 경우 자신의 블록(영역) 내에서 선언한 변수의 우선순위가 높다.

--- Finals_2023_1S/no_2.c
+++ Finals_2023_1S/no_2.c
@@ -8,9 +8,11 @@ int Max(int a, int b, int c){
     return max;
 }
 
-int mani(void){
+int main(void){
     int max = 0;
-    Max(3,4);
+
+    //함수 인자값 추가
+    Max(3,4,1);
     printf("가장 큰 수는 %d입니다.", max);
 
     return 0;
