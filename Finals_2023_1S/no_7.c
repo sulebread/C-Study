b3d1c7dd010680ad3c024c7e1f73ce69a5b1997e
haky <57565164+sulebread@users.noreply.github.com>
2023-06-11 18:25:15 +0900

    7번 문제 답
    답 : 배열의 원소를 하나씩 대입하여 복사한다.

--- Finals_2023_1S/no_7.c
+++ Finals_2023_1S/no_7.c
@@ -7,8 +7,13 @@ int main(void){
     
     //잘못된 부분 : 배열의 이름은 배열의 시작 주소값을 가지고 있는 값으로 다른 배열의 주소를 대입할 수 없다.
     //포인터 상수 ?
-    brr = arr;
+    //brr = arr;
 
+    //배열복사
+    for(i = 0; i < 5; i++)
+        brr[i] = arr[i];
+
+    //배열출력
     for(i = 0; i < 5; i++)
         printf("%d %d \n", arr[i], brr[i]);
 
