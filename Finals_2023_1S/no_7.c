c1c0d65d225146b96089af8088b7fdf0a8d1ec08
haky <57565164+sulebread@users.noreply.github.com>
2023-06-11 18:22:46 +0900

    7번 문제
    문제 : 배열 전체 값을 복사하는 코드이다. 코드를 보고 잘못된 부분의 이유를 설명하시오.

---
+++ Finals_2023_1S/no_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int arr[5] = {1, 2, 3, 4, 5};
+    int brr[5] = {10, 20, 30, 40, 50};
+    int i;
+    
+    //잘못된 부분 : 배열의 이름은 배열의 시작 주소값을 가지고 있는 값으로 다른 배열의 주소를 대입할 수 없다.
+    //포인터 상수 ?
+    brr = arr;
+
+    for(i = 0; i < 5; i++)
+        printf("%d %d \n", arr[i], brr[i]);
+
+    return 0;
+}
