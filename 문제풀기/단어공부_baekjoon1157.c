a06e6cfb3cfcc07766e20e49e54d1093b1147b1b
haky <57565164+sulebread@users.noreply.github.com>
2023-07-19 01:46:32 +0900

    Update 단어공부_baekjoon1157.c
    나중에 다시 풀어보기 ㅠ

--- 문제풀기/단어공부_baekjoon1157.c
+++ 문제풀기/단어공부_baekjoon1157.c
@@ -1,33 +1,55 @@
 #include <stdio.h>
 
 int main(void){
+    int max = -1, index, check = 0;
     char result = 0;
     char arr[1000000]= {'0',};
-    int count[1000000] = {0,};
+    int count[26] = {0,};
 
-    //int count2[26] = {0,};
-
+    //문자입력
     scanf("%s", arr);
 
+    //대문자 범위에 있거나, 소문자 범위에 있는 경우
     for(int i = 0; i < 1000000; i++){
-        for(int j = 0; j < 1000000; j++){
-            if(arr[i] == arr[j] || arr[i] == arr[j] + 32 || arr[i] == arr[j] - 32){
-                count[i]++;
+        if((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z')){
+            int alphabet = 0;
+
+            //알파벳이 소문자인 경우 대문자로 전환
+            if(arr[i] > 'Z'){
+                arr[i] -= 32;
             }
+
+            //arr[i]에 들어있는 알파벳 번호 구하고, count 하기
+            alphabet = arr[i] - 'A';
+            count[alphabet]++;
+        }else{
+            break;
+        }
+        
+    }
+    
+    //max = count가 가장 큰 숫자 구하기. 
+    //max count의 알파벳 번호 저장하기.
+    for(int i = 0; i < 26; i++){
+        if(count[i] > max){
+            max = count[i];
+            index = i;
         }
     }
 
-    for(int i = 0; i < 1000000; i++){
-        if(count[i] > count[i+1])
-            result = arr[i];
-        else if(count[i] < count[i+1])
-            result = arr[i+1];
-        else if(count[i] == count[i+1])
-            result = 63;
+    //count가 가장 큰 숫자가 여러개 있는 경우 ? 출력.
+    for(int i = 0; i < 26; i++){
+        if(count[i] == max && i != index){
+            printf("?");
+            check = 1;
+            break;
+        }
     }
 
-
-    printf("%c", result);
+    //count가 가장 큰 숫자가 하나만 있는 경우 알파벳 번호 아스키 코드화하기.
+    if(check != 1){
+        printf("%c", index + 'A');
+    }
 
     return 0;
 }
