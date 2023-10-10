0a7bab360135e11a1bdc666d1bf3eaaefda4d0dc
haky <57565164+sulebread@users.noreply.github.com>
2023-10-10 15:15:24 +0900

    Create No_01.c
    크기가 3인 double형 배열의 모든 원소의 주소를 출력하는 프로그램을 작성하시오. 단, 주소 구하기 연산자를 사용하지 마시오.

---
+++ Chapter8/No_01.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+//배열의 주소를 인자로 받는 함수 선언
+void printAddress(double *arr, int size);
+
+int main(void){
+    double arr[3] = {0, }; 
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    //배열이름(주소)와 사이즈를 함수로 전달
+    printAddress(arr, size); 
+ 
+    return 0;
+}
+
+void printAddress(double *arr, int size){
+    //배열을 포인터처럼 사용하여 주소출력
+    for(int i = 0; i < size; i++){
+        printf("arr[%d]의 주소 : %X\n", i, arr + i);
+    }
+}
